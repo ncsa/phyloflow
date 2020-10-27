45b40df7ad01f76a38151e6d993e28aed1d92932
Peter Groves <pdgroves@gmail.com>
2020-10-27 13:39:16 +0000

    hello world wdl file

--- pyclone/code/pyclone_entrypoint.sh
+++ pyclone/code/pyclone_entrypoint.sh
@@ -1,5 +1,5 @@
 ## This is the script that runs *inside* the container and receives the arguments
 ## from the 'docker run' command to pass to the pyclone executable
-echo "hello from inside the pyclone container. Does conda work:"
+echo "hello from inside the pyclone container. Does conda work?:"
 conda info
 echo "entrypoint.sh complete"
