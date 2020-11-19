27a2aca0ea2aa67096563f7a5c5f21853f1e599e
Peter Groves <pdgroves@gmail.com>
2020-11-19 16:10:37 -0600

    added a filtered vcf file that should take much less time to cluster

--- run_vcf_transform_example1.sh
+++ run_vcf_transform_example1.sh
@@ -6,5 +6,5 @@
 # Run vcf_transform against the example data that it comes with. Will produce outputs for
 # both pyclone and pyclone_vi to input.
 
-FILE=./example_data/mek_lab_vcfs/A25.mutect2.vcf
+FILE=./example_data/mek_lab_vcfs/A25.mutect2.filtered.snp.vcf
 miniwdl run --dir=runs/ vcf_transform/vcf-transform-task.wdl vcf_type=mutect vcf_file=$FILE
