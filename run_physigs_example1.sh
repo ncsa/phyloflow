062fe20999aa53481686ed754f7bf07505998325
ChuanyiZ <chuanyi5@illinois.edu>
2021-05-17 09:00:39 -0500

    Add example data for physigs

---
+++ run_physigs_example1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PHYSIGS_TREE=./example_data/physigs/tree.csv
+PHYSIGS_SNV=./example_data/physigs/snv.csv
+PHYSIGS_SIGNATURES=./example_data/physigs/signatures.txt
+
+miniwdl run --dir=runs/ physigs/physigs-task.wdl \
+    tree_csv=$PHYSIGS_TREE \
+    snv_csv=$PHYSIGS_SNV \
+    signatures=$PHYSIGS_SIGNATURES
+
