3ffaec91411947ced78f8bd7056638bee3f09ebc
ChuanyiZ <chuanyi5@illinois.edu>
2021-05-17 08:47:29 -0500

    Add parameter alpha for tail probability

--- cluster_transform/code/cluster_transform_entrypoint.sh
+++ cluster_transform/code/cluster_transform_entrypoint.sh
@@ -4,18 +4,19 @@
 # This script expects two args
 # $1 cluster_type: pyclone or pyclon-vi
 # $2 cluster_file: path to a cluster_file produced by the pipeline type 'cluster_type'
-# $3 output_file: data file input to SPRUCE
-# $4 pyclone_vi_input_file: only used for pyclone-vi, used for extracting VAF
+# $3 alpha: tail probability, lower and uppder bound of VAF takes [alpha/2, 1 - alpha/2]
+# $4 output_file: data file input to SPRUCE
+# $5 pyclone_vi_input_file: only used for pyclone-vi, used for extracting VAF
 #		Note that when using inside of wdl, wdl will change the filename and mount
 #		it into the container for you (and that is the version this script will recieve)
 
 echo "cluster_transform_entrypoint.sh: begin."
 echo "cluster_transform_entrypoint.sh: list of input args from wdl runtime: $@"
-if [ $# -eq 3 ]; then
-    conda run -n cluster-transform python -B -m py_code.main -t $1 -c $2 -o $3
-elif [ $# -eq 4 ]; then
-    conda run -n cluster-transform python -B -m py_code.main -t $1 -c $2 -o $3 -v $4
+if [ $# -eq 4 ]; then
+    conda run -n cluster-transform python -B -m py_code.main -t $1 -c $2 -a $3 -o $4
+elif [ $# -eq 5 ]; then
+    conda run -n cluster-transform python -B -m py_code.main -t $1 -c $2 -a $3 -o $4 -v $5
 else
-    echo "cluster_transform_entrypoint.sh: wrong number of input args, only 3 or 4 allowed."
+    echo "cluster_transform_entrypoint.sh: wrong number of input args, only 4 or 5 allowed."
 fi
 echo "cluster_transform_entrypoint.sh: finished."
--- run_cluster_transform.sh
+++ run_cluster_transform.sh
@@ -7,8 +7,10 @@
 # both pyclone and pyclone_vi to input.
 
 PYCLONE_VI_CLUSTER=./example_data/cluster/pyclone_vi_AML_cluster_assignment.tsv
+ALPHA=0.05
 PYCLONE_VI_INPUT=./example_data/cluster/pyclone_vi_AML_input.tsv
 miniwdl run --dir=runs/ cluster_transform/cluster-transform-task.wdl \
     cluster_type=pyclone-vi \
     cluster_tsv_file=$PYCLONE_VI_CLUSTER \
+    alpha=$ALPHA \
     pyclone_vi_formatted=$PYCLONE_VI_INPUT
