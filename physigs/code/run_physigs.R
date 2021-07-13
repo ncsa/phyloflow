f0a3be3d038be34220d9fcadd8b6dcfd31ce7a9a
ChuanyiZ <chuanyi5@illinois.edu>
2021-07-12 23:26:04 -0500

    update physigs task, improve script to support named arguments

--- physigs/code/run_physigs.R
+++ physigs/code/run_physigs.R
@@ -2,53 +2,64 @@ library(PhySigs, warn.conflicts = FALSE)
 library(graph, warn.conflicts = FALSE)
 library(Rgraphviz, warn.conflicts = FALSE)
 library(RColorBrewer, warn.conflicts = FALSE)
+library(optparse, warn.conflicts = FALSE)
 
-args <- commandArgs(trailingOnly = TRUE)
-if (length(args) == 3 || length(args) == 4) {
-    tree_file = args[1]
-    snv_file = args[2]
-    o_pdf_file = base::paste(args[3], "plot.pdf", sep='.')
-    o_tree_file = base::paste(args[3], "tree.tsv", sep='.')
-    o_exposure_file = base::paste(args[3], "exposure.tsv", sep='.')
-    if (length(args) == 4) {
-       S = readLines(args[4])
-    } else {
-        S <- c(
-            "Signature.1",
-            "Signature.2",
-            "Signature.3",
-            "Signature.4",
-            "Signature.5",
-            "Signature.6",
-            "Signature.7",
-            "Signature.8",
-            "Signature.9",
-            "Signature.10",
-            "Signature.11",
-            "Signature.12",
-            "Signature.13",
-            "Signature.14",
-            "Signature.15",
-            "Signature.16",
-            "Signature.17",
-            "Signature.18",
-            "Signature.19",
-            "Signature.20",
-            "Signature.21",
-            "Signature.22",
-            "Signature.23",
-            "Signature.24",
-            "Signature.25",
-            "Signature.26",
-            "Signature.27",
-            "Signature.28",
-            "Signature.29",
-            "Signature.30"
-        )
-    }
+option_list <- list(
+    make_option(c("--tree"), action = "store", type = "character",
+        default = NULL, help = "tree CSV file"),
+    make_option(c("--snv"), action = "store", type = "character",
+        default = NULL, help = "SNV CSV file"),
+    make_option(c("--outprefix"), action = "store", type = "character",
+        default = NULL, help = "output file name prefix"),
+    make_option(c("--signature"), action = "store", type = "character",
+        default = NULL, help = "selected signatures file, each line contains a signature name"),
+    make_option(c("--hg38"), action = "store_true",
+        default = FALSE,
+        help = "set flag to use reference hg38, [default FALSE]")
+)
+
+opt <- parse_args(OptionParser(option_list = option_list))
+
+tree_file = opt$tree
+snv_file = opt$snv
+o_pdf_file = base::paste(opt$outprefix, "plot.pdf", sep='.')
+o_tree_file = base::paste(opt$outprefix, "tree.tsv", sep='.')
+o_exposure_file = base::paste(opt$outprefix, "exposure.tsv", sep='.')
+if (!is.null(opt$signature)) {
+    S = readLines(opt$signature)
 } else {
-    cat("Usage: run_physigs.R TREE SNV OUTPUT_PREFIX [SIGNATURES]\nOutput: OUTPUT_PREFIX.plot.pdf, \n")
-    q(status=1)
+    S <- c(
+        "Signature.1",
+        "Signature.2",
+        "Signature.3",
+        "Signature.4",
+        "Signature.5",
+        "Signature.6",
+        "Signature.7",
+        "Signature.8",
+        "Signature.9",
+        "Signature.10",
+        "Signature.11",
+        "Signature.12",
+        "Signature.13",
+        "Signature.14",
+        "Signature.15",
+        "Signature.16",
+        "Signature.17",
+        "Signature.18",
+        "Signature.19",
+        "Signature.20",
+        "Signature.21",
+        "Signature.22",
+        "Signature.23",
+        "Signature.24",
+        "Signature.25",
+        "Signature.26",
+        "Signature.27",
+        "Signature.28",
+        "Signature.29",
+        "Signature.30"
+    )
 }
 
 # Input CSV file
@@ -74,6 +85,9 @@ for (i in 1:nrow(tree_matrix)){
 # snv_file <- system.file("extdata", "snv.csv", package = "PhySigs", mustWork = TRUE)
 input_mat <- as.data.frame(read.csv(file=snv_file,
                                     colClasses = c("character", "character", "numeric", "character", "character")))
+if (!startsWith(input_mat$chr, "chr")) {
+    input_mat$chr <- paste("chr", input_mat$chr, sep='')
+}
 
 # Use deconstructSigs to convert SNVs to 96 Features
 P <- deconstructSigs::mut.to.sigs.input(mut.ref = input_mat,
@@ -81,6 +95,7 @@ P <- deconstructSigs::mut.to.sigs.input(mut.ref = input_mat,
                                         chr = "chr",
                                         pos = "pos",
                                         ref = "ref",
+                                        # bsg = BSgenome.Hsapiens.UCSC.hg38,
                                         alt = "alt")
 
 # Normalize feature matrix
